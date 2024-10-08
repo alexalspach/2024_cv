8cbf6d8c0afb6745779c553944383727310e23fd
Alex Alspach <alexalspach@gmail.com>
2024-10-08 01:32:13 -0400

    permalinks, smooth scrolling, and animations

--- site/assets/styles/js/include_html.js
+++ site/assets/styles/js/include_html.js
@@ -1,44 +1,43 @@
+function loadContentAndScroll(tabId, anchorId) {
+  var contentDivId;
+  var contentUrl;
+  switch (tabId) {
+    case 'projects':
+      contentDivId = '#includeProjects';
+      contentUrl = 'projects.html';
+      break;
+    case 'work':
+      contentDivId = '#includeExperience';
+      contentUrl = 'experience.html';
+      break;
+    case 'education':
+      contentDivId = '#includeEducation';
+      contentUrl = 'education.html';
+      break;
+    case 'publications':
+      contentDivId = '#includePapers';
+      contentUrl = 'paperspatents.html';
+      break;
+    case 'me':
+      contentDivId = '#includeMe';
+      contentUrl = 'me.html';
+      break;
+    // Add other cases as needed
+    default:
+      return;
+  }
 
-
-$(function(){
-  $("#includeMe").load("me.html"); 
-});
-
-$(function(){
-  $("#includeContact").load("contact.html"); 
-});
-
-$(function(){
-  $("#includeEducation").load("education.html"); 
-});
-
-$(function(){
-  $("#includeExperience").load("experience.html"); 
-});
-
-$(function(){
-  //$("#includePapers").load("papers.html"); 
-  $("#includePapers").load("paperspatents.html"); 
-});
-
-$(function(){
-  $("#includePresentations").load("presentations.html"); 
-});
-
-//name of page may change 
-$(function(){
-  $("#includeProjects").load("projects.html"); 
-});   
-
-$(function(){
-  $("#includeVideos").load("videos.html"); 
-});
-
-$(function(){
-  $("#includeSmallWorld").load("smallWorld.html"); 
-});
-
-$(function(){
-  $("#includeStage").load("stage.html"); 
-}); 
-
+  $(contentDivId).load(contentUrl, function () {
+    if (anchorId) {
+      var target = $('#' + anchorId);
+      if (target.length) {
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
+      }
+    }
+  });
+}
